024fa09aaf7b367bc71d02555f38d13c37fa1991
dang khoi <s4015143@rmit.edu.vn>
2024-05-19 01:08:24 +0700

    create post dark mode update almost finished

--- public/javascript/createpost.js
+++ public/javascript/createpost.js
@@ -1,17 +1,30 @@
+let themeStylesheet = document.getElementById('quill-theme-stylesheet');
 
 
 const quill = new Quill('#editor', {
-    modules: 
-    {
+  modules: {
     toolbar: '#toolbar-container'
-    },
-    placeholder: 'Compose an epic...',
-    theme: 'snow'
+  },
+placeholder: 'Compose an epic...',
+  theme: 'snow'
 });
 
-
-var text = quill.getText();
-
+// Create a new MutationObserver instance
+let observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (mutation.attributeName === "class") {
+        let classList = mutation.target.classList;
+        if (classList.contains('dark')) {
+          themeStylesheet.href = "/public/css/quill.smoke.css";
+        } else {
+          themeStylesheet.href = "https://cdn.jsdelivr.net/npm/quill@2.0.2/dist/quill.snow.css";
+        }
+      }
+    });
+  });
+  
+  // Start observing the body element for attribute changes
+  observer.observe(body, { attributes: true });
 
 document.querySelector('form').addEventListener('submit', function(e) {
     var hiddenInput = document.createElement('input');
