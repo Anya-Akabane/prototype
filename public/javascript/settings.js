5e7fdc43c511105a187face5211a953edc91ee3a
Walnut <s4020232@rmit.edu.vn>
2024-05-18 11:19:15 +0700

    yes

--- public/javascript/settings.js
+++ public/javascript/settings.js
@@ -14,22 +14,42 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
 
   if (!name) {
     errorMessage += "Name field must be filled out!\n";
+    document.getElementById('name').classList.add('error');
+  } else {
+    document.getElementById('name').classList.remove('error');
+    document.getElementById('name').classList.add('success');
   }
 
   if (!nickName) {
     errorMessage += "Nickname field must be filled out!\n";
+    document.getElementById('nickName').classList.add('error');
+  } else {
+    document.getElementById('nickName').classList.remove('error');
+    document.getElementById('nickName').classList.add('success');
   }
 
   if (!bio) {
     errorMessage += "Bio field must be filled out!\n";
+    document.getElementById('bio').classList.add('error');
+  } else {
+    document.getElementById('bio').classList.remove('error');
+    document.getElementById('bio').classList.add('success');
   }
 
   if (!gender) {
     errorMessage += "Gender field must be filled out!\n";
+    document.getElementById('genders').classList.add('error');
+  } else {
+    document.getElementById('genders').classList.remove('error');
+    document.getElementById('genders').classList.add('success');
   }
 
   if (!birthdate) {
     errorMessage += "Birthdate field must be filled out!\n";
+    document.getElementById('birthdate').classList.add('error');
+  } else {
+    document.getElementById('birthdate').classList.remove('error');
+    document.getElementById('birthdate').classList.add('success');
   }
 
   if (errorMessage) {
